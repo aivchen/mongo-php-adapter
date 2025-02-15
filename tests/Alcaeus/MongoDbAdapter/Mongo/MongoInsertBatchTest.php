a32e2dfd14a0ecdab76edf89c10de1034b5c6714
Andrei Ivchenkov <a.ivchenkov@volta.space>
2025-02-15 23:19:46 +0300

    added sleep for async batch operations

--- tests/Alcaeus/MongoDbAdapter/Mongo/MongoInsertBatchTest.php
+++ tests/Alcaeus/MongoDbAdapter/Mongo/MongoInsertBatchTest.php
@@ -46,6 +46,7 @@ class MongoInsertBatchTest extends TestCase
         ];
 
         $this->assertSame($expected, $batch->execute(['w' => 0]));
+        sleep(1);
 
         $newCollection = $this->getCheckDatabase()->selectCollection('test');
         $this->assertSame(2, $newCollection->count());
